ac1e5505e91d9193246531ff501b5c7531628762
stefanovagloria <gloriqstefanova97@gmail.com>
2024-02-25 21:00:36 +0300

    HTML changes added

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,21 @@
+import styles from './Profile.module.css';
+import img from './../../assets/userImg.png';
+
 const Profile = () => {
 
     return(
-        <h1>Profile Page</h1>
+        <div className={styles.container}>
+            <div>
+            <img src={img} className={styles.img}/>
+            </div>
+            <div>
+                Name
+            </div>
+            <div>
+                Email
+            </div>
+            <button>Edit profile</button>
+        </div>
     )
 }
 
